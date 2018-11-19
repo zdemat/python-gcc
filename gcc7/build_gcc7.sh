0bbd28cc298f3494332c99943a80fbbd9dadd712
Mads R. B. Kristensen <madsbk@gmail.com>
2018-11-19 14:17:15 +0100

    test

--- gcc7/build_gcc7.sh
+++ gcc7/build_gcc7.sh
@@ -5,18 +5,14 @@ set -e
 set -x
 
 # Install gcc7 (/opt/gcc7)
-pwd
-ls
 tar -xf deps/gcc-7.3.0.tar.xz
-ls
-ls deps
-cd gcc-7.3.0
-mv ../deps/* .
-ls
+mv gcc-7.3.0 ~/bh/gcc
+mv deps/* ~/bh/gcc
+cd ~/bh/gcc
 ./contrib/download_prerequisites --no-graphite
 mkdir -p ~/gcc_build
 cd ~/gcc_build
-~/gcc-7.3.0/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls
+~/gcc/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls
 make
 make install
 
