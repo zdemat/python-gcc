1c34866e93a77a7729ef88a16f0f01a4f5afaf57
Mads R. B. Kristensen <madsbk@gmail.com>
2018-11-19 15:40:47 +0100

    test

--- gcc7/build_gcc7.sh
+++ gcc7/build_gcc7.sh
@@ -12,7 +12,12 @@ cd ~/gcc_src
 ./contrib/download_prerequisites --no-graphite
 mkdir ~/gcc_build
 cd ~/gcc_build
-~/gcc_src/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls
-make
+~/gcc_src/configure --prefix ~/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls &> log.txt
+make &> log.txt
 make install
+cd ~/gcc7_install/
+ls -Rlh
+du -sh
+
+
 
