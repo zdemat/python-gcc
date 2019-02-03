c071fcfc548c4efb5a9a0bcb59bf39147cc124cb
Zdeněk Matěj <zdenek.matej@maxiv.lu.se>
2019-02-03 21:32:48 +0100

    stopping removal of files from deps during build

--- gcc7/build_gcc7.sh
+++ gcc7/build_gcc7.sh
@@ -7,7 +7,7 @@ set -x
 # Install gcc7 (/opt/gcc7)
 tar -xf deps/gcc-7.3.0.tar.xz
 mv gcc-7.3.0 ~/gcc_src
-mv deps/* ~/gcc_src/
+cp deps/* ~/gcc_src/
 cd ~/gcc_src
 ./contrib/download_prerequisites --no-graphite
 mkdir ~/gcc_build
